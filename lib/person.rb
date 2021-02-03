97624063aa896c189b2d4ac0c82ee48afe702b51
guilhermezils <guilhermezils@icloud.com>
2021-02-03 15:34:15 -0600

    commit tree c82585b67ab18460e17765f4c64242f5cafb4903

--- lib/person.rb
+++ lib/person.rb
@@ -4,23 +4,34 @@
 
 class Person
 
-@@all =[]
+
     attr_reader :name, :bank
     attr_accessor :bank, :happiness, :hygiene
     # bank = 25
     # (o..11)
    
-    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+    def initialize (name)
         @name = name
-        @bank = bank_account
-        @happiness = happiness
-        @hygiene = hygiene
-
-        @@all << self
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
 
     end
 
-    def 
+    def happiness= (new_value)
+        if @happiness < 0
+            @happiness = 0
+        elsif @happiness > 10
+            @happiness = 10
+        end
+
+    end
+    
+
+
+
+    
+
 
 end
     
