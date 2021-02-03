5d7b970c1f4b2e5227ba4fc8241485043fb66b2f
guilhermezils <guilhermezils@icloud.com>
2021-02-03 14:47:17 -0600

    added changes1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # your code goes here
+
+class Person
+@@all =[]
+    attr_reader :name, :bank
+    attr_accessor :bank, :happiness, :hygiene
+    # bank = 25
+    # (o..11)
+   
+    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+
+        @@all << self
+
+    end
+
+    
+
+
+
+
+
+
+end
